e889b962943a762f8b6f5a8b84244625cfbbbb2c
Vinay Kumar <vinay.kumar@kartrocket.com>
2019-08-31 16:23:50 +0530

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 
-const routes: Routes = [];
+const routes: Routes = 
+  [{
+    path: '',
+    component: MyCounterComponent
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
